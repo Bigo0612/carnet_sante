9113cd50b09c9a1f2debecbf84f3a34681b3231a
valentin <laidet.val@hotmail.fr>
2019-11-11 19:05:14 +0100

    font responsive flowtype js

--- assets/front/js/main.js
+++ assets/front/js/main.js
@@ -3,4 +3,12 @@ $(window).load(function() {
 	animation: "slide",
 	controlNav: false,
 	});
+
+	$('body').flowtype({
+		minimum : 480,
+ 		maximum : 1280,
+		minFont : 8,
+ 		maxFont : 16,
+		fontRatio: 60,
+	});
 });
