2eb7b6fc359122b59a8bee4c7599a9c75b762f61
valentin <laidet.val@hotmail.fr>
2019-11-28 15:10:38 +0100

    finition page profil

--- carnet.php
+++ carnet.php
@@ -1,13 +1,25 @@
+<?php include "header.php";?>
 
+<div class="profil">
 
+  <div class="profilmichel">
+    <img class="photofred" src="assets/front/img/frédéricmichel.jpg" alt="photo_de_Frédéric_Michel">
+    <p class="profilnom">Frédéric Michel</p>
+    <img class="photoforet" src="assets/front/img/montagne.jpg" alt="photo_foret">
+  </div>
 
-<?php include "header.php";
-include('assets/front/inc/functions.php');
-include('assets/front/inc/pdo.php');
-?>
+    <ul class="dropmenu">
+        <li><a href="#">Ajouter un vaccin</a></li><hr>
+        <i class="fas fa-syringe dropvaccins"></i>
+        <li><a href="#">Vaccination de voyage</a></li><hr>
+        <i class="fas fa-plane dropvoyage"></i>
+        <li><a href="#">Se proteger et proteger les autres</a></li><hr>
+        <i class="fas fa-heartbeat dropproteger"></i>
+        <li><a href="#">Editer mon profil</a></li><hr>
+        <i class="far fa-edit dropprofil"></i>
+    </ul>
+</div>
 
-<link rel="stylesheet" href="assets/front/css/carnet.css">
-<div class="blockblanc"></div>
 <div class="boxtableau">
 
   <table class="tableaucarnet">
